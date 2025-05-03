ed85644d659db11b6d1e0135de099fcebb21ddff
kain <battlekain@mail.ru>
2025-05-03 21:13:34 +0400

    download image

--- models/Article.php
+++ models/Article.php
@@ -39,11 +39,11 @@ class Article extends \yii\db\ActiveRecord
     public function rules()
     {
         return [
-            [['title', 'description', 'content', 'date', 'image', 'viewed', 'user_id', 'status', 'category_id'], 'default', 'value' => null],
-            [['description', 'content'], 'string'],
-            [['date'], 'safe'],
-            [['viewed', 'user_id', 'status', 'category_id'], 'integer'],
-            [['title', 'image'], 'string', 'max' => 255],
+            [['title'], 'required'],
+            [['title', 'description', 'content'], 'string'],
+            [['date'], 'date', 'format' => 'php:Y-m-d'],
+            [['date'], 'default', 'value' => date('Y-m-d')],
+            [['title'], 'string', 'max' => 255],
         ];
     }
 
@@ -66,24 +66,28 @@ class Article extends \yii\db\ActiveRecord
         ];
     }
 
-    /**
-     * Gets query for [[ArticleTags]].
-     *
-     * @return \yii\db\ActiveQuery
-     */
-    public function getArticleTags()
+    public function getImage()
     {
-        return $this->hasMany(ArticleTag::class, ['article_id' => 'id']);
+        return ($this->image) ? '/uploads/' .  $this->image : '/no-image.png';
     }
 
-    /**
-     * Gets query for [[Comments]].
-     *
-     * @return \yii\db\ActiveQuery
-     */
-    public function getComments()
+    public function saveImage($filename)
     {
-        return $this->hasMany(Comment::class, ['article_id' => 'id']);
+        $this->image = $filename;
+        return $this->save(false);
     }
+
+    public function deleteImage()
+    {
+        $imageUploadModel = new ImageUpload();
+        $imageUploadModel->deleteCurrentImage($this->image);
+    }
+
+    public function beforeDelete()
+    {
+        $this->deleteImage();
+        return parent::beforeDelete();
+    }
+
 
 }
