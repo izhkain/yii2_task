f7178d12dcf21d50b17cce96212647419d43a418
kain <battlekain@mail.ru>
2025-05-07 03:03:51 +0400

    setting categories

--- models/Category.php
+++ models/Category.php
@@ -44,4 +44,9 @@ class Category extends \yii\db\ActiveRecord
         ];
     }
 
+    public function getArticles()
+    {
+        return $this->hasMany(Article::className(), ['category_id' => 'id']);
+    }
+
 }
